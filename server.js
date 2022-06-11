c04e3d1539801fa4f66860a2e34fd944d43c7921
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-06-10 22:36:44 -0700

    added functions to inquirer choices

--- server.js
+++ server.js
@@ -1,42 +1,87 @@
-const inquirer = require("inquirer");
-const mysql = require("mysql2");
-const cTable = require("console.table");
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const cTable = require('console.table');
+const figlet = require('figlet');
 
-require("dotenv").config();
+require('dotenv').config();
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
+  host: 'localhost',
+  user: 'root',
   password: process.env.DB_PASSWORD,
-  database: "team",
+  database: 'team',
 });
-
-const dbActions = () => {
-  inquirer
-    .prompt({
-      type: "list",
-      name: "firstAction",
-      message: "Waht would you like to do?",
-      choices: [
-        "View All Employees",
-        "View all Roles",
-        "View all Departments",
-        "Add Role",
-        "Add Department",
-        "add Employees",
-        "Update Employee Role",
-        "Quit"
-      ],
-    })
-    .then((answer) => {
-      console.log(answer);
-    });
-};
 
 db.connect((err) => {
   if (err) throw err;
-
-  dbActions();
+  managerPrompt();
 
   // console.log("Sucessfully connected")
 });
+
+figlet('EMPLOYEE MANAGER', function(err, data) {
+  if (err) {
+    console.log('Something went wrong...');
+    console.dir(err);
+    return;
+  }
+  console.log(data)
+});
+
+const managerPrompt = () => {
+  inquirer
+    .prompt([
+      {
+      type: 'list',
+      name: 'firstAction',
+      message: 'Waht would you like to do?',
+      choices: [
+        'View All Employees',
+        'View all Roles',
+        'View all Departments',
+        'Add Role',
+        'Add Department',
+        'Add Employees',
+        'Update Employee Role',
+        'Update Employee Manager',
+        // 'View All Employees be Department',
+
+        'Quit'
+      ],
+    }
+  ])
+    .then((answers) => {
+      const { choices } = answers;
+
+      if (choices === 'View All Employees') {
+        viewEmployees();
+      }
+      if (choices === 'View All Roles') {
+        viewRoles();
+      }
+      if (choices === 'View all Departments') {
+        viewDepartments();
+      }
+      if (choices === 'Add Role') {
+        addRole();
+      }
+      if (choices === 'Add Department') {
+        addDepartment();
+      }
+      if (choices === 'Add Employees') {
+        addEmployee();
+      }
+      if (choices === 'Update Employee Role') {
+        updateEmployee();
+      }
+      if (choices === 'Update Employee Manager') {
+        updateManager();
+      }
+      if (choices === 'Quit') {
+        db.end();
+      };
+      // console.log(answers);
+    });
+};
+
+
